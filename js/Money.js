c9e6bd1ac96716a6b745fe0382cb6ab577e09675
Jodyburne <j__richardson@live.co.uk>
2019-06-20 15:02:25 +0100

    updated the coin

--- js/Money.js
+++ js/Money.js
@@ -6,7 +6,7 @@ class Money {
         this.y = 1200
         this.vy = 8
         this.img = new Image()
-        this.img.src = "images/coinMaybs1.gif"
+        this.img.src = "images/coin_01.png"
         this.audioClip = new Audio()
         this.audioClip.src = "cha ching.mp3"
         this.audioClip.volume = 0.5
@@ -15,7 +15,7 @@ class Money {
     draw(ctx) {
         ctx.save()
         ctx.beginPath();
-        ctx.globalAlpha = 0
+        ctx.globalAlpha = 1
         ctx.fillStyle = "green"
         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         ctx.stroke();
@@ -23,7 +23,7 @@ class Money {
         ctx.restore()
 
         ctx.save()
-        ctx.drawImage(this.img, this.x - (this.radius * 2.8), this.y - (this.radius * 2.8), 150, 150)
+        ctx.drawImage(this.img, this.x - (this.radius), this.y - (this.radius), 60, 60)
         ctx.translate(this.x, this.y)
         ctx.restore()
     }
